a8a332a43b6c1fc4e8db49365069d414209cc5c3
StonedWolfy <48760278+StonedWolfy@users.noreply.github.com>
2019-04-09 21:07:29 +0300

    Day Two JS

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const
   switcher = document.querySelector('#cbx'),
   more = document.querySelector('.more'),
   modal = document.querySelector('.modal'),
-  videos = document.querySelectorAll('.videos__items');
+  videos = document.querySelectorAll('.videos__item');
 
   let player;
 
@@ -104,5 +104,77 @@ more.addEventListener('click', () =>{
       setTimeout(()=>{
         card.classList.remove('videos__item-active');
       },10)
+      bindNewModal(card);
   }
+   sliceTitel('.videos__item-descr', 99);
 });
+
+function sliceTitel(selector, count) {
+   document.querySelectorAll(selector).forEach(item => {
+     item.textContent.trim();
+     if(item.textContent.length < count) {
+      return;
+     } else{
+      const str = item.textContent.slice(0, count + 1) + "...";
+      item.textContent = str;
+     }
+   })
+}
+sliceTitel('.videos__item-descr', 100);
+
+function openModal() {
+  modal.style.display = 'block';
+}
+
+function closeModal() {
+  modal.style.display = 'none';
+  player.stopVideo();
+}
+
+function bindModal(card) {
+  card.forEach(item =>{
+    item.addEventListener('click', (e) =>{
+      e.preventDefault();
+      const id = item.getAttribute('data-url');
+      loadVideo(id);
+      openModal();
+    });
+  });
+}
+bindModal(videos);
+
+function bindNewModal(card){
+   card.addEventListener('click', (e)=>{
+    e.preventDefault();
+    const id = card.getAttribute('data-url');
+    loadVideo(id);
+    openModal();
+   });
+}
+modal.addEventListener('click', (e) => {
+   if(!e.target.classList.contains('modal__body')){
+      closeModal();
+   }
+});
+
+function createVideo() {
+  var tag = document.createElement('script');
+
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  
+  setTimeout(() => {
+      player = new YT.Player('frame', {
+      height: '100%',
+      width: '100%',
+      videoId: 'M7lc1UVf-VE'
+    });
+  }, 300);
+}
+
+createVideo();
+
+function loadVideo(id) {
+    player.loadVideoById({'videoId': `${id}`});
+}
